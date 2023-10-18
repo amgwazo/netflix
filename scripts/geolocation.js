52a3eb30faa626f2d8dc13351e1cb7f552449f1d
amgwazo <amgwazo@hotmail.com>
2023-10-18 14:42:50 +0200

    Added Country to the Get location Pop up message

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -1,6 +1,9 @@
 let x = document.getElementById("out");
 let y = document.getElementById("weather");
 
+const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+
+
 function geolocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -24,8 +27,18 @@ function showPosition(data) {
     .then((data) => {
       console.log(data);
       let cityName = data.city.name;
+      let countryCode = data.city.country;
+      let country = regionNames.of(countryCode);
       let temp = data.list[0].temp.day + " °C";
-    //   y.innerText = `Weather of ${cityName} is ${temp}`;
-      alert(`Weather of ${cityName} is ${temp}`);
+      //   y.innerText = `Weather of ${cityName} is ${temp}`;
+      console.log(countryCode);
+      console.log(regionNames.of(countryCode)); // 👉️ "United States"
+      alert(
+        `The temperature in your area  (${cityName} -  ${country}) is ${temp}.`
+      );
     });
 }
+
+
+
+// module.export 

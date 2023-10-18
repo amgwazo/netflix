685377ad5a3668a3f4f036d69144a1df2150b379
amgwazo <amgwazo@hotmail.com>
2023-10-18 08:25:45 +0200

    Added Coupon Code logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
+
+const randomCode = () => {
+    let rnm = Math.random().toString(36).slice(2);
+    console.log(rnm);
+    return rnm.toUpperCase();
+}
+
+function onloadPage() {
+  console.log('onloadPage');
+  document.getElementById("coupon").style.visibility = "visible";
+  document.getElementById("header").style.opacity = "0.7";
+
+  let promocode = document.getElementById("promocode");
+  promocode.innerText = randomCode();
+}
+
+window.onload = onloadPage();
+
+function closeCoupon() {
+  document.getElementById("coupon").style.visibility = "hidden";
+  document.getElementById("search").style.opacity = "1";
+}
+
 let btn = document.getElementById("btn");
 
 const changeMode = () => {
-
   if (btn.innerText === "Light Mode") {
     btn.innerText = "Dark Mode";
     btn.style.color = "#9fa2a6";
@@ -44,7 +66,7 @@ const changeMode = () => {
   lightSectionKids.classList.toggle("light-section");
   lightSectionFaq.classList.toggle("light-faq-section");
 
-  lightFooter.classList.toggle('lightTheme');
+  lightFooter.classList.toggle("lightTheme");
 
   //   lightSectionEnjoyTv.classList.toggle("light-section");
   //   lightSectionDownload.classList.toggle("light-section");
